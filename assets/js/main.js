2b6a6878e6e9121632fbfeb598483083962cc4fe
Edilando <35576130+EdilandoSaturnino@users.noreply.github.com>
2022-04-15 23:42:10 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+
+$("#valorInicial, #ipi, #st, #icms, #porcentagemLucro").keyup(function(){
+
+var total = 0;    	
+var valorInicial = Number($("#valorInicial").val());
+var ipi = Number($("#ipi").val());
+var st = Number($("#st").val());
+var icms = Number($("#icms").val());
+var porcentagemLucro = Number($("#porcentagemLucro").val());
+
+var total= (valorInicial + (ipi / 100) + (st / 100) + (icms / 100)) * porcentagemLucro;
+$('#total').val(total);
+
+});
+});
