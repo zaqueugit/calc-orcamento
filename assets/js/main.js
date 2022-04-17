50754151bdad8e6d6af6c51d8344643557426df2
Edilando <35576130+EdilandoSaturnino@users.noreply.github.com>
2022-04-17 09:57:42 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 $(function() {
 
-$("#valorInicial, #ipi, #st, #icms, #porcentagemLucro").keyup(function(){
+$("#valorinicial, #ipi, #st, #icms, #porcentagemlucro").keyup(function(){
 
 var total = 0;    	
-var valorInicial = Number($("#valorInicial").val());
+var valorInicial = Number($("#valorinicial").val());
 var ipi = Number($("#ipi").val());
 var st = Number($("#st").val());
 var icms = Number($("#icms").val());
-var porcentagemLucro = Number($("#porcentagemLucro").val());
+var porcentagemLucro = Number($("#porcentagemlucro").val());
 
 var total = (valorInicial + (ipi / 100) + (st / 100) + (icms / 100)) * porcentagemLucro;
 $('#total').val(parseFloat(total.toFixed(2)));
